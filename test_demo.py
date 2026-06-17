98e6f01f88bdb0116457eac6a796b557303e1f98
Pranab Ghosh <68161399+pranabcoder@users.noreply.github.com>
2026-06-17 10:45:04 +0000

    2nd commit

--- test_demo.py
+++ test_demo.py
@@ -2,7 +2,7 @@ import re
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
      # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
